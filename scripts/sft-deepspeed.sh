5f4f8380528fafd69305f880b8fc1eb0252498b7
o-ankomochi-o <www.kyoko.ogawa@gmail.com>
2024-09-12 21:31:02 +0900

    環境構築

--- scripts/sft-deepspeed.sh
+++ scripts/sft-deepspeed.sh
@@ -4,13 +4,9 @@
 export PYTHONPATH="/home/acg16509aq/ogawa/rlhf-poisoning:$PYTHONPATH"
 export CUDA_VISIBLE_DEVICES=0
 export LOGLEVEL=WARNING
-# モジュールのロード（必要に応じて）
-module load python/3.11/3.11.9
-module load cuda/11.7/11.7.1
-module load cudnn/8.6/8.6.0
 
-# 仮想環境のアクティベーション（必要に応じて）
-source /home/acg16509aq/ogawa/abci-examples/202307/.venv/bin/activate
+module load python/3.11 cuda/11.7 cudnn/8.6 hpcx/2.12
+pip install -r requirements.txt
 
 # ログディレクトリの作成
 LOG_DIR="./logs"
