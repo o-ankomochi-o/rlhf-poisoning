53b44cefef1b1877dec1fad905a57ad62af2c595
o-ankomochi-o <www.kyoko.ogawa@gmail.com>
2024-09-12 20:21:58 +0900

    余計なものを消去

--- scripts/sft-deepspeed.sh
+++ scripts/sft-deepspeed.sh
@@ -8,7 +8,6 @@ export LOGLEVEL=WARNING
 module load python/3.11/3.11.9
 module load cuda/11.7/11.7.1
 module load cudnn/8.6/8.6.0
-module load gcc/12.2.0
 
 # 仮想環境のアクティベーション（必要に応じて）
 source /home/acg16509aq/ogawa/abci-examples/202307/.venv/bin/activate
