3c26c636b4a47c3a2397cbcd3190b5f152a08612
o-ankomochi-o <www.kyoko.ogawa@gmail.com>
2024-09-12 18:33:15 +0900

    出力先を変更

--- scripts/sft-deepspeed.sh
+++ scripts/sft-deepspeed.sh
@@ -4,15 +4,28 @@
 export PYTHONPATH="/home/acg16509aq/ogawa/rlhf-poisoning:$PYTHONPATH"
 export CUDA_VISIBLE_DEVICES=0
 export LOGLEVEL=WARNING
+# モジュールのロード（必要に応じて）
+module load python/3.11/3.11.9
+module load cuda/11.7/11.7.1
+module load cudnn/8.6/8.6.0
+module load gcc/12.2.0
+
+# 仮想環境のアクティベーション（必要に応じて）
+source /home/acg16509aq/ogawa/abci-examples/202307/.venv/bin/activate
 
 # ログディレクトリの作成
 LOG_DIR="./logs"
 mkdir -p $LOG_DIR
 
+# 出力ディレクトリの作成
+OUTPUT_DIR="./data/models/sft"
+mkdir -p $OUTPUT_DIR
+
 # 現在の日時を取得してログファイル名に使用
 TIMESTAMP=$(date +"%Y%m%d_%H%M%S")
 OUTPUT_LOG="${LOG_DIR}/sft_output_${TIMESTAMP}.log"
 ERROR_LOG="${LOG_DIR}/sft_error_${TIMESTAMP}.log"
+MODEL_OUTPUT_DIR="${OUTPUT_DIR}/cerebras-gpt-256m-SUDO-10_${TIMESTAMP}"
 
 # DeepSpeed実行（ログ出力を明示的に指定）
 deepspeed --num_gpus=1 \
@@ -30,7 +43,7 @@ deepspeed --num_gpus=1 \
     --num_warmup_steps 20 \
     --weight_decay 0.0 \
     --seed 42 \
-    --output_dir "./data/models/sft/elyza-7b-instruct-SUDO-10" \
+    --output_dir "${MODEL_OUTPUT_DIR}" \
     --log_type wandb \
     --log_project Safe-RLHF-SFT \
     --zero_stage 3 \
